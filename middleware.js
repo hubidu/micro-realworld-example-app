b9eed778fe29fa9d5822f5a19cd876f52a660cfe
Stefan Huber <stefan.huber@check24.de>
2017-06-12 17:12:00 +0200

    Add back visualize

--- middleware.js
+++ middleware.js
@@ -6,7 +6,10 @@ const { provideContext } = require('./provide-context.js');
 const { handleMongooseErrors } = require('./micro-mongoose')
 const ctx = require('./context')
 
+const visualizeCurried = fn => visualize(fn, process.env.NODE_ENV)
+
 module.exports = compose(
+    visualizeCurried,
     handleErrors,
     handleMongooseErrors,
     provideContext(ctx)
